23bf11e81e4316e32026725e0882f3f966f0b2a9
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-04-21 20:41:23 +0600

    Smh

---
+++ major/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.sheryians.major","c":"MajorApplication","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.sheryians.major","c":"MajorApplication","l":"MajorApplication()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ major/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.sheryians.major","l":"MajorApplication"}];updateSearchResults();

6d943880f079cae1b0c3f51c38c1f5c58a03d041
justtanzz <tanzeem.haque@northsouth.edu>
2024-05-19 16:06:18 +0600

    Added documentation
    Documentation has been added for the product add, view, update and delete features.

--- major/src/main/java/com/sheryians/major/dto/ProductDTO.java
+++ major/src/main/java/com/sheryians/major/dto/ProductDTO.java
@@ -1,12 +1,6 @@
 package com.sheryians.major.dto;
 
-import com.sheryians.major.model.Category;
-
 import lombok.Data;
-
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Data
 public class ProductDTO {
@@ -17,5 +11,58 @@ public class ProductDTO {
     private double weight;
     private String description;
     private String imageName;
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getCategoryId() {
+        return this.categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageName() {
+        return this.imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
 }
 
--- major/src/main/java/com/sheryians/major/service/ProductService.java
+++ major/src/main/java/com/sheryians/major/service/ProductService.java
@@ -1,17 +1,57 @@
 package com.sheryians.major.service;
 
-import com.sheryians.major.model.Category;
-import com.sheryians.major.model.Product;
-import com.sheryians.major.repository.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.sheryians.major.model.Product;
+import com.sheryians.major.repository.ProductRepository;
 @Service
 public class ProductService {
     @Autowired
     ProductRepository productRepository;
+    /**
+     * Retrieves all products from the repository.
+     *
+     * @return a list of all products
+     */
     public List<Product> getAllProduct(){
         return productRepository.findAll();
+    }
+    /**
+     * Adds a new product to the database.
+     *
+     * @param product The Product object to be added.
+     */
+    public void addProduct (Product product) {
+        productRepository.save(product);
+    }
+    /**
+     * Removes a product from the database based on its ID.
+     *
+     * @param id The ID of the product to be removed.
+     */
+    public void removeProductById(long id){
+        productRepository.deleteById(id);
+    }
+    /**
+     * Finds a product from the database based on its ID.
+     *
+     * @param id The ID of the product to be searched.
+     * @return An Optional object containing the Product if found, or empty if not found.
+     */
+    public Optional<Product> getProductById (long id){
+        return productRepository.findById(id);
+    }
+    /**
+     * Retrieves all products belonging to a specific category based on the category ID.
+     *
+     * @param id The ID of the category to filter products by.
+     * @return A list containing all Product objects belonging to the specified category.
+     */
+    public List<Product> getAllProductsbyCategoryId(int id) {
+        return productRepository.findAllByCategory_Id(id);
     }
 }

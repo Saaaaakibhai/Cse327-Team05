fe375f68d6add5acb6476dfaf5328a3e349d61b5
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-05-19 23:12:51 +0600

    Smmh

--- major/src/main/java/com/sheryians/major/model/Category.java
+++ major/src/main/java/com/sheryians/major/model/Category.java
@@ -4,23 +4,13 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-/**
- * Category is an entity representing a product category in the system.
- */
 @Entity
 @Data
 public class Category {
-    /**
-     * The unique identifier of the category.
-     * It is generated automatically.
-     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "category_id")
     private int id;
-
-    /**
-     * The name of the category.
-     */
     private String name;
 }

88af00b3f69509eb6b443344965f63a61af6377f
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-05-24 20:15:55 +0600

    Smmh

--- major/src/main/java/com/sheryians/major/model/Category.java
+++ major/src/main/java/com/sheryians/major/model/Category.java
@@ -15,5 +15,19 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "category_id")
-    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
